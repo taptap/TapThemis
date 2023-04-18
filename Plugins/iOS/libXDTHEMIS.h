c4aae3eb3b289a01e38bdd8ce14396b993b4ab22
ci-gitlab <tds-developer@xd.com>
2023-04-18 14:57:42 +0800

    feat:update upm

---
+++ Plugins/iOS/libXDTHEMIS.h
@@ -0,0 +1,39 @@
+//
+//  libXDCrash.h
+//  libXDCrash
+//
+//  Created by tianguo on 2021/9/28.
+//
+
+#import <Foundation/Foundation.h>
+
+
+
+@interface libXDTHEMIS : NSObject
+#ifdef __cplusplus
+extern "C" {
+#endif
+//extern void _lib_init_THEMIS_agent();
+    
+extern void _lib_init_THEMIS_agent_with_id(const char* appId);
+extern void _lib_report_custom_exception(const char* name, const char* reason, const char* stackTrace, bool isQuitApp);
+extern void _lib_report_exception_with_type(const char* name, const char* reason,const char* stackTrace, int exception_type, bool bIsQuit);
+extern void _lib_report_custom_exception_ex(const char* name, const char* reason,const char* message,bool bIsQuit,const char* extra_message,int extra_len);
+extern void _lib_set_native_callback(long themis_state_cb,long extra_message_cb);
+extern void _add_custom_field(const char * str_key,const char * str_value);
+    
+    
+    
+extern void _lib_event_tracking(const char *logData);
+    
+extern void _lib_log_report(const char *logMessage);
+extern void _lib_debug_log(const char *lpLogData);
+    extern void _lib_enable_debug_mode(bool isTurnOn);
+
+
+        
+    
+#ifdef __cplusplus
+}
+#endif
+@end
